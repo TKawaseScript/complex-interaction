edbabd63c6e2534df64646b99b6e503911896001
TKawase <110525228+TKawaseScript@users.noreply.github.com>
2025-02-05 12:56:47 +0900

    modified:   script/TableS4 and S5.R

--- script/TableS4 and S5.R
+++ script/TableS4 and S5.R
@@ -1,6 +1,8 @@
 library(MuMIn)
+library(VGAM)
+library(dplyr)
 
-#TableS6
+#TableS4
 degree_dist_All_all_digdis<-read.csv("degree_dist_All_all_digdis.csv")
 sp_food_coltp0<-read.csv("spcollisttp=0.csv",header=T,fileEncoding = "UTF-8")
 degree_dist_out_digdis<-read.csv("degree_dist_out_digdis.csv")
@@ -16,19 +18,21 @@ for(i in 1:length(degree_dist_All_all_digdis$X)){
   foods[i]<-sp_food_coltp0[sp_food_coltp0$cause==degree_dist_All_all_digdis$X[i],]$foodhabit7
 }
 
-Each_Other_lm_data<-data.frame("count"=degree_dist_All_all_digdis$x,"popmean"=popmeans,"food"=foods)
 
-glm_Each_Other<-glm(count~popmean*food,data=Each_Other_lm_data,family="poisson")
+Each_Other_lm_data<-data.frame("countCause"=degree_dist_out_digdis$x,"counteffect"=degree_dist_in_digdis$x,"popmean"=popmeans,"food"=foods)
 
-options(na.action = "na.fail")  # 欠損値がある場合にエラーを出す設定
-
-Each_all_models <- dredge(glm_Each_Other)
-
-write.csv(capture.output(Each_all_models),"Each_all_models_num_interact.csv")
+#igraphから抜き出している場合種名の順番が因果関係の原因側でも結果側でも同じため以下のような式になる
+glm_Each_Other_all<-vglm(cbind(Each_Other_lm_data$countCause,Each_Other_lm_data$counteffect)~popmean+food,data=Each_Other_lm_data,family=poissonff)
+glm_Each_Other_food<-vglm(cbind(Each_Other_lm_data$countCause,Each_Other_lm_data$counteffect)~food,data=Each_Other_lm_data,family=poissonff)
+glm_Each_Other_popmean<-vglm(cbind(Each_Other_lm_data$countCause,Each_Other_lm_data$counteffect)~popmean,data=Each_Other_lm_data,family=poissonff)
+AIC(glm_Each_Other_all)
+AIC(glm_Each_Other_food)
+AIC(glm_Each_Other_popmean)
 
 # AICが最小のモデルを表示
-Each_best_model <- get.models(Each_all_models, subset = 1)[[1]]
-summary(Each_best_model)
+summary(glm_Each_Other_popmean)
+
+write.csv(capture.output(summary(glm_Each_Other_popmean)),"Each_all_models_num_interact.csv")
 
 
 summary(glm(count~popmean,data=Each_Other_lm_data,family="poisson"))
@@ -53,12 +57,12 @@ glm_Out<-glm(count~popmean*food,data=Out_lm_data,family="poisson")
 
 
 Out_all_models <- dredge(glm_Out)
-write.csv(capture.output(Out_all_models),"Out_all_models_num_interact.csv")
-
 
 # AICが最小のモデルを表示
 Out_best_model <- get.models(Out_all_models, subset = 1)[[1]]
 summary(Out_best_model)
+
+write.csv(capture.output(summary(Out_best_model)),"Out_all_models_num_interact.csv")
 
 
 summary(glm(count~popmean,data=Out_lm_data,family="poisson"))
@@ -83,16 +87,79 @@ glm_In<-glm(count~popmean*food,data=In_lm_data,family="poisson")
 
 In_all_models <- dredge(glm_In)
 
-write.csv(capture.output(In_all_models),"In_all_models_num_interact.csv")
 
 
 # AICが最小のモデルを表示
 In_best_model <- get.models(In_all_models, subset = 1)[[1]]
 summary(In_best_model)
 
+write.csv(capture.output(summary(In_best_model)),"In_all_models_num_interact.csv")
+
+
 
 summary(glm(count~popmean,data=In_lm_data,family="poisson"))
 summary(glm(count~food,data=In_lm_data,family="poisson"))
 
+
+#TableS5 媒介中心生モデルのものも追加して解析を行う
+TableS1data<-read.csv("TableS1.csv")
+
+glmAICdataCen <-subset(TableS1data,TableS1data$Centrality.betweenness!= "-")
+
+glmAIC_dataFrame<-data.frame("Abbreviation"=glmAICdataCen$Abbreviation,"centrality"=glmAICdataCen$`Centrality.betweenness`,"popmean"=glmAICdataCen$mean,"food"=glmAICdataCen$Food.habit)
+
+glmAIC_dataFrame_Outname<-NULL
+for(i in 1:nrow(degree_dist_out_digdis)){
+  glmAIC_dataFrame_Outname<-c(glmAIC_dataFrame_Outname,sp_food_coltp0[degree_dist_out_digdis$X[i]==sp_food_coltp0$cause,]$bindre.namesp)
+}
+
+degree_dist_out_digdis$X<-glmAIC_dataFrame_Outname
+
+glmAIC_dataFrame_Inname<-NULL
+for(i in 1:nrow(degree_dist_in_digdis)){
+  glmAIC_dataFrame_Inname<-c(glmAIC_dataFrame_Inname,sp_food_coltp0[degree_dist_in_digdis$X[i]==sp_food_coltp0$cause,]$bindre.namesp)
+}
+
+degree_dist_in_digdis$X<-glmAIC_dataFrame_Inname
+colnames(degree_dist_in_digdis)<-c("Abbreviation","in_count")
+colnames(degree_dist_out_digdis)<-c("Abbreviation","out_count")
+
+mergeData_in<-merge(glmAICdataCen, degree_dist_in_digdis, by = "Abbreviation", all.x = TRUE)
+
+mergeData_in_out<-merge(mergeData_in, degree_dist_out_digdis, by = "Abbreviation", all.x = TRUE)
+
+
+
+#0.000001のような小さい値を足す
+mergeData_in_out$Centrality.betweenness<-as.numeric(mergeData_in_out$Centrality.betweenness)
+
+mergeData_in_out$mean<-as.numeric(mergeData_in_out$mean)
+
+
+glm_Out_cent<-glm(Centrality.betweenness~mean*Food.habit,data=mergeData_in_out,family="gamma")
+
+
+Out_all_models_cent <- dredge(glm_Out_cent)
+
+Out_best_model_cent <- get.models(Out_all_models_cent, subset = 1)[[1]]
+summary(Out_best_model_cent)
+
+
+glm_In_cent<-glm(Centrality.betweenness~mean*Food.habit,data=mergeData_in_out,family="gamma")
+
+
+In_all_models_cent <- dredge(glm_In_cent)
+
+In_best_model_cent <- get.models(In_all_models_cent, subset = 1)[[1]]
+summary(In_best_model_cent)
+
+
+
+glm_Each_Other_all_cent<-vglm(cbind(mergeData_in_out$out_count,mergeData_in_out$in_count)~mean+Food.habit,data=mergeData_in_out,family=)
+glm_Each_Other_food_cent<-vglm(cbind(mergeData_in_out$out_count,mergeData_in_out$in_count)~Food.habit,data=mergeData_in_out,family=)
+glm_Each_Other_popmean_cent<-vglm(cbind(mergeData_in_out$out_count,mergeData_in_out$in_count)~mean,data=mergeData_in_out,family=)
+AIC(glm_Each_Other_all_cent)
+AIC(glm_Each_Other_food_cent)
+AIC(glm_Each_Other_popmean_cent)
 
 
